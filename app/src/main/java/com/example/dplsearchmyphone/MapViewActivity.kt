449729f135a48b1bc15a94c9a6678230c8d9879c
Ffice <onbazooka@gmail.com>
2023-10-26 17:59:12 +0300

    v1

---
+++ app/src/main/java/com/example/dplsearchmyphone/MapViewActivity.kt
@@ -0,0 +1,11 @@
+package com.example.dplsearchmyphone
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MapViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_map_view)
+    }
+}
